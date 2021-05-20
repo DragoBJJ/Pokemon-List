169fb9d5095f946691a8df531dddfe3a24238195
Kurpiewski, Lukasz <luk.kurpiewski@gmail.com>
2021-05-20 18:24:24 +0200

    Add sample folder structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
+import SearchOverlay from './components/SearchOverlay';
+
 function App() {
   return (
     <div className="App">
@@ -15,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <SearchOverlay />
         </a>
       </header>
     </div>
